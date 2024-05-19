046e066f0981bad1c578c040492467f9cab443a9
Alexander Paz <alexjpaz@gmail.com>
2024-05-19 13:49:54 -0400

    Change test to fail

--- games/simple-gut/test/unit/test_sample.gd
+++ games/simple-gut/test/unit/test_sample.gd
@@ -4,5 +4,4 @@ func test_pass():
 	assert_true(true, "Should pass, true is true")
 	
 func test_fail():
-	pending
-	# assert_true(false, "Should pass, true is true")
+	assert_true(false, "Should pass, true is true")
