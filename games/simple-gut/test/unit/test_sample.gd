e91a99c83d9c7fb39d566d7611dd36c2ce0d43ef
Alexander Paz <alexjpaz@gmail.com>
2024-05-19 10:45:00 -0400

    Fixing asserting for GUT

--- games/simple-gut/test/unit/test_sample.gd
+++ games/simple-gut/test/unit/test_sample.gd
@@ -4,4 +4,4 @@ func test_pass():
 	assert_true(true, "Should pass, true is true")
 	
 func test_fail():
-	assert_true(true, "Should pass, true is true")
+	assert_true(false, "Should pass, true is true")
