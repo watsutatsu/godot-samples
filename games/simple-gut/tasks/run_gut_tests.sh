2d08070bde077d0b4aa1c3efef472ff886ec4093
Alexander Paz <alexjpaz@gmail.com>
2024-05-19 13:47:24 -0400

    Do weird hack to import project
    refs https://github.com/watsutatsu/godot-samples/issues/3#issuecomment-2119308212

--- games/simple-gut/tasks/run_gut_tests.sh
+++ games/simple-gut/tasks/run_gut_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-timeout 5s godot --headless -e
+# HACK - See https://github.com/watsutatsu/godot-samples/issues/3#issuecomment-2119308212
+timeout 30s godot --headless --import-stuff --editor --path "$PWD" 
+
 godot --headless -s addons/gut/gut_cmdln.gd --path "$PWD" -gdir=res://test/unit -glog=1 -gexit
