21ec3721439ba170a3ae970f3f28f404223fc999
Alexander Paz <alexjpaz@gmail.com>
2024-05-19 13:51:52 -0400

    set testing to pending again

--- games/simple-gut/test/unit/test_sample.gd
+++ games/simple-gut/test/unit/test_sample.gd
@@ -4,4 +4,5 @@ func test_pass():
 	assert_true(true, "Should pass, true is true")
 	
 func test_fail():
-	assert_true(false, "Should pass, true is true")
+	pending
+	# assert_true(false, "Should pass, true is true")
