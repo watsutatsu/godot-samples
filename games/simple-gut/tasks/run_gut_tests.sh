76906891f6104f1676d5f4e2ae2ab4921080915d
Alexander Paz <alexjpaz@gmail.com>
2024-05-19 13:20:35 -0400

    remove -d

--- games/simple-gut/tasks/run_gut_tests.sh
+++ games/simple-gut/tasks/run_gut_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-godot --headless -s addons/gut/gut_cmdln.gd -d --path "$PWD" -gdir=res://test/unit -glog=1 -gexit
+godot --headless -s addons/gut/gut_cmdln.gd --path "$PWD" -gdir=res://test/unit -glog=1 -gexit
